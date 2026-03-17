e73e8d20405a9766a45ae0c07e767a49094aec14
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-03-16 22:09:48 -0500

    Lab5 3

---
+++ function_types.py
@@ -0,0 +1,7 @@
+def list_shift(números, valor):
+    for i in range(len(números)):
+        números[i] += (valor)
+def calc_avg(números):
+    return sum(números) / len(números)
+def print_normalized(lista):
+    print(lista)
